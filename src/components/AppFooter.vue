2ac56f845ccecafaec0f57ab1f2a548b9f938a21
canhtv05 <canhtvph51036@gmail.com>
2025-03-16 01:22:05 +0700

    customize UI

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,14 +1,10 @@
 <template>
   <CFooter class="px-4">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ms-1"
-        >&copy; {{ new Date().getFullYear() }} creativeLabs.</span
-      >
+      <span>SD19303_Agile_Group6</span>
     </div>
     <div class="ms-auto">
-      <span class="me-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <span>Copyright CoreUI and rebuilt by Canhtv05 x Khanh</span>
     </div>
   </CFooter>
 </template>
